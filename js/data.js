92d48a2cce774f6e13fb6c052a1a4f9cec46cf44
breezeparadon <breezeparadon@gmail.com>
2024-07-15 22:46:16 +0700

    Update

---
+++ js/data.js
@@ -0,0 +1,10 @@
+const data = {
+    data1: "Each summer, elephants roam freely.",
+    data2: "Finding a way, we gathered courage.",
+    data3: "Gracefully, he danced under moonlight.",
+    data4: "Apples are abundant in autumn.",
+    data5: "Beneath the stars, dreams unfold.",
+    data6: "Determined to succeed against all odds."
+};
+
+export default data;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
+import data from './data.js';
+
+console.log(data);
+
+function sortDataAZ() {
+    let sortedData = Object.keys(data).sort((a, b) => {
+        if (typeof data[a] === 'string' && typeof data[b] === 'string') {
+            return data[a].localeCompare(data[b], 'th', { sensitivity: 'base' });
+        }
+        return 0;
+    });
+    updateDataList(sortedData);
+}
+
+function sortDataZA() {
+    let sortedData = Object.keys(data).sort((a, b) => {
+        if (typeof data[a] === 'string' && typeof data[b] === 'string') {
+            return data[b].localeCompare(data[a]);
+        } else {
+            return 0;
+        }
+    });
+    updateDataList(sortedData);
+}
+
+
+function sortDataNumberDesc() {
+    let sortedData = Object.keys(data).sort((a, b) => data[b] - data[a]);
+    updateDataList(sortedData);
+}
+
+function sortDataNumberAsc() {
+    let sortedData = Object.keys(data).sort((a, b) => data[a] - data[b]);
+    updateDataList(sortedData);
+}
+
+function updateDataList(sortedData) {
+    sortedData.forEach((key, index) => {
+        document.getElementById(`data${index + 1}`).textContent = data[key];
+    });
+}
+
+document.getElementById('sortByAZ').addEventListener('click', sortDataAZ);
+document.getElementById('sortByZA').addEventListener('click', sortDataZA);
+document.getElementById('sortByNumberDesc').addEventListener('click', sortDataNumberDesc);
+document.getElementById('sortByNumberAsc').addEventListener('click', sortDataNumberAsc);
+
+document.getElementById('data1').textContent = data.data1;
+document.getElementById('data2').textContent = data.data2;
+document.getElementById('data3').textContent = data.data3;
+document.getElementById('data4').textContent = data.data4;
+document.getElementById('data5').textContent = data.data5;
+document.getElementById('data6').textContent = data.data6;
+
+
 function Opensearch() {
     var searchForm = document.querySelector('.searchbar-show');
 
